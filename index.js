2e6d1e9d7e0019ee2c7dc776c0792d8850c54f2d
NandoPla <fernandopla8@gmail.com>
2021-09-04 03:15:50 -0300

    feat: jS

---
+++ index.js
@@ -0,0 +1,37 @@
+const cartaUm = document.getElementById("carta-1")
+const cartaDois = document.getElementById("carta-2")
+const cartaTres = document.getElementById("carta-3")
+const cartaQuatro = document.getElementById("carta-4")
+const cartaCinco = document.getElementById("carta-5")
+const cartaSeis = document.getElementById("carta-6")
+const cartaSete = document.getElementById("carta-7")
+const cartaOito = document.getElementById("carta-8")
+const cartaNove = document.getElementById("carta-9")
+const cartaDez = document.getElementById("carta-10")
+const cartaOnze = document.getElementById("carta-11")
+const cartaDoze = document.getElementById("carta-12")
+const cartaTreze = document.getElementById("carta-13")
+const cartaCatorze = document.getElementById("carta-14")
+const cartaQuinze = document.getElementById("carta-15")
+const cartaDezesseis = document.getElementById("carta-16")
+const cartaDezessete = document.getElementById("carta-17")
+const cartaDezoito = document.getElementById("carta-18")
+const cartaDezenove = document.getElementById("carta-19")
+const cartaVinte = document.getElementById("carta-20")
+
+var img_name = new Array("carta1");
+
+var l = img_name.length;
+
+var rnd_no = Math.floor(l*Math.random());
+
+document.r_img.src = img_name[rnd_no];
+
+<img src="purple.gif" width="100" height="50" 
+name="r_img" alt="Random image" />
+
+//cartaUm.onclick = function(){
+//    cartaUm.setAttribute ()
+//}
+
+
